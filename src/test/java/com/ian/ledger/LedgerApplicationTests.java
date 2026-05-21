057867e26c0c77a7001f37b4172118388bcaf56e
Ian Montes <ianpdmontes@gmail.com>
2026-05-21 10:17:15 +0300

    feat: scaffolding initial Spring Boot project

---
+++ src/test/java/com/ian/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ian.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
